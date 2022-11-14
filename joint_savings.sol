85a5588944f84868c954b7d6ffc2ff261f7b4a95
Michael <mblauberg@outlook.com>
2022-11-14 19:59:46 +1000

    final

--- joint_savings.sol
+++ joint_savings.sol
@@ -26,7 +26,11 @@ contract JointSavings {
     - A variable of type `address public` named `lastToWithdraw`
     - Two variables of type `uint public` named `lastWithdrawAmount` and `contractBalance`.
     */
-    // YOUR CODE HERE!
+    address payable accountOne;
+    address payable accountTwo;
+    address public lastToWithdraw;
+    uint public lastWithdrawAmount;
+    uint public contractBalance;
 
     /*
     Define a function named **withdraw** that will accept two arguments.
@@ -38,26 +42,32 @@ contract JointSavings {
         /*
         Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. The `requiere` statement returns the text `"You don't own this account!"` if it does not.
         */
-        // YOUR CODE HERE!
+        require(recipient == accountOne || recipient == accountTwo, "You don't own this account!");
 
         /*
-        Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
+        Define a require statement that checks if balance is sufficient for accomplishing the withdrawal operation. 
+        If insufficient funds exist, it will return the “Insufficient funds!” text.
         */
-        // YOUR CODE HERE!
+        require(contractBalance >= amount, "Insufficient funds!");
 
         /*
-        Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
+        Add an if statement to check if lastToWithdraw is not equal to (!=) recipient. 
+        If it isn’t equal, set it to the current value of recipient.
         */
-        // YOUR CODE HERE!
+        if(lastToWithdraw != recipient) {
+            lastToWithdraw = recipient;
+        }
 
-        // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
-        // YOUR CODE HERE!
+        
+        //Call the transfer function of the recipient, and pass it the amount to transfer as an argument.
+        recipient.transfer(amount);
 
-        // Set  `lastWithdrawAmount` equal to `amount`
-        // YOUR CODE HERE!
+        //Set lastWithdrawAmount equal to amount
+        lastWithdrawAmount = amount;
 
-        // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
-        // YOUR CODE HERE!
+        
+        //Set the contractBalance variable equal to the balance of the contract by using address(this).balance to reflect the new balance of the contract.
+        contractBalance = address(this).balance;
     }
 
     // Define a `public payable` function named `deposit`.
@@ -66,7 +76,7 @@ contract JointSavings {
         /*
         Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
         */
-        // YOUR CODE HERE!
+        contractBalance = address(this).balance;
     }
 
     /*
@@ -75,11 +85,14 @@ contract JointSavings {
     function setAccounts(address payable account1, address payable account2) public{
 
         // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
-        // YOUR CODE HERE!
+        accountOne = account1;
+        accountTwo = account2;
     }
 
     /*
     Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
     */
-    // YOUR CODE HERE!
+    function() external payable {
+        contractBalance = address(this).balance;
+    }
 }
